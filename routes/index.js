2c2098693d792ab49663da4e30aa7dd77966ebd8
ttrampp <ttrampp@msn.com>
2025-05-12 11:31:09 -0600

    connected database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {res.send('Hello World')});
+//router.get('/', (req, res) => {res.send('Hello World')});
+
+router.use('/contacts', require('./contacts'));
+
+//router.use('/users', require('./users'));
 
 module.exports = router;
